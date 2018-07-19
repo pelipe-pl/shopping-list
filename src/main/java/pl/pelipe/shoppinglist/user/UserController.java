260bc86859ca60b9130e7f14e672b309b97682d5
Filip Andersen <filipandersen@outlook.com>
2018-07-19 16:50:40 +0200

    UserController refactored.

--- src/main/java/pl/pelipe/shoppinglist/user/UserController.java
+++ src/main/java/pl/pelipe/shoppinglist/user/UserController.java
@@ -1,18 +1,14 @@
 package pl.pelipe.shoppinglist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import pl.pelipe.shoppinglist.item.ItemDto;
-import pl.pelipe.shoppinglist.item.ItemService;
 
 import java.security.Principal;
-import java.util.List;
 
 @Controller
 public class UserController {
