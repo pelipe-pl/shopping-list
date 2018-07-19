277e18ec86bf91d4f75ca4a26ad3c152848c3387
Filip Andersen <filipandersen@outlook.com>
2018-07-19 17:56:31 +0200

    Email service added.

---
+++ src/main/java/pl/pelipe/shoppinglist/email/EmailService.java
@@ -0,0 +1,37 @@
+package pl.pelipe.shoppinglist.email;
+
+import com.sendgrid.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class EmailService {
+
+    private final Environment environment;
+
+    @Autowired
+    public EmailService(Environment environment) {
+        this.environment = environment;
+    }
+
+    public void send(String to, String subject, String content) throws IOException {
+        Email from = new Email(environment.getProperty("SENDGRID_FROM_EMAIL"));
+        Mail mail = new Mail(from, subject, new Email(to), new Content("text/plain", content));
+        SendGrid sendGrid = new SendGrid(environment.getProperty("SENDGRID_API_KEY"));
+        Request request = new Request();
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            Response response = sendGrid.api(request);
+            System.out.println(response.getStatusCode());
+            System.out.println(response.getBody());
+            System.out.println(response.getHeaders());
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
