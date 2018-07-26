1f99994d3e2cd6a5f50d187255fc78d0db38d8cf
Filip Andersen <filipandersen@outlook.com>
2018-07-26 15:08:33 +0200

    Password reset token added.

---
+++ src/main/java/pl/pelipe/shoppinglist/user/PasswordResetToken.java
@@ -0,0 +1,22 @@
+package pl.pelipe.shoppinglist.user;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class PasswordResetToken {
+
+    private static final int EXPIRATION = 60 * 24;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String token;
+
+    @OneToOne(targetEntity = UserEntity.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "user_id")
+    private UserEntity user;
+
+    private LocalDateTime expiryDate;
+}
