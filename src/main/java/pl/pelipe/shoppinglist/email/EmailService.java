6a0deed9ed1fbaac48cac22a6b6538d402ed383b
pelipe-pl <filipandersen@outlook.com>
2019-01-10 03:16:43 +0100

    Sharing item list status change email sending added

--- src/main/java/pl/pelipe/shoppinglist/email/EmailService.java
+++ src/main/java/pl/pelipe/shoppinglist/email/EmailService.java
@@ -6,9 +6,12 @@ import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import pl.pelipe.shoppinglist.item.ItemEntity;
 import pl.pelipe.shoppinglist.item.ItemListDto;
+import pl.pelipe.shoppinglist.item.ItemListEntity;
+import pl.pelipe.shoppinglist.user.UserEntity;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class EmailService {
@@ -40,10 +43,8 @@ public class EmailService {
     }
 
     public Boolean sendItemList(String to, ItemListDto itemList, List<ItemEntity> items) {
-
         StringBuilder stringList = new StringBuilder();
         stringList.append("<STRONG>").append(itemList.getName().toUpperCase()).append("</STRONG><br>");
-
         for (
                 ItemEntity item : items) {
             if (item.getDone()) {
@@ -54,6 +55,34 @@ public class EmailService {
 
         send(to, "Shopping list: " + itemList.getName().toUpperCase(), stringList.toString());
         return true;
+    }
+
+    public void sendItemListShareConfirmation(UserEntity listSharer, ItemListEntity itemList) {
+        String to = listSharer.getUsername();
+        String listSharerName = listSharer.getName();
+        String listOwnerName = itemList.getUser().getName();
+        String listName = itemList.getName();
+        String subject = listOwnerName + " just shared a shopping list with you!";
+        String content = "Hello " + listSharerName + "! <BR><BR>" +
+                "User " + listOwnerName + " shared a shopping list named " + listName.toUpperCase() + "." + "<BR>" +
+                "You can log in to your Shopping List account and check it out. <BR><BR>" +
+                "Thank you.";
+        send(to, subject, content);
+    }
+
+    public void sendItemListStopSharingConfirmation(Set<UserEntity> sharedWithUsers, ItemListEntity itemList) {
+        for (UserEntity listSharer : sharedWithUsers) {
+            String to = listSharer.getUsername();
+            String listSharerName = listSharer.getName();
+            String listOwnerName = itemList.getUser().getName();
+            String listName = itemList.getName();
+            String subject = listOwnerName + " stopped sharing a shopping list with you!";
+            String content = "Hello " + listSharerName + "! <BR><BR>" +
+                    "User " + listOwnerName + " just stopped sharing his/her shopping list named " + listName.toUpperCase() + "." + "<BR>" +
+                    "You will no longer be able to see it when you log in to your Shopping List account.<BR><BR>" +
+                    "Thank you.";
+            send(to, subject, content);
+        }
     }
 }
 
